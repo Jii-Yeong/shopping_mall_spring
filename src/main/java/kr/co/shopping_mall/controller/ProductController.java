dbd5e34e550fe475ced0b684561b276ef0612eea
user <qqs2615@naver.com>
2021-05-22 21:08:57 +0900

    메인 홈페이지 방문시 방문자 수 증가

--- src/main/java/kr/co/shopping_mall/controller/ProductController.java
+++ src/main/java/kr/co/shopping_mall/controller/ProductController.java
@@ -1,21 +1,30 @@
 package kr.co.shopping_mall.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.co.shopping_mall.service.ProductService;
+import kr.co.shopping_mall.service.VisitorService;
 
 
 @Controller
 public class ProductController {
 	@Autowired
 	private ProductService service;
+	@Autowired
+	private VisitorService visitorService;
+	
 	@GetMapping("/")
 	public String index(Model model) {
+		String today = LocalDate.now().format(DateTimeFormatter.ofPattern("MM월 dd일"));
+		visitorService.ExistToday(today);
+		visitorService.VisitorUpdateCnt(today);
 		model.addAttribute("product_list", service.readAll());
 		return "index";
-		
 	}
 }
